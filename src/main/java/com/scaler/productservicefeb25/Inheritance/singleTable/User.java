8d7a25d4e01f7990976fabc6014e7fc1205f4d46
Arzoo Khan <khanarzoo617@gmail.com>
2025-02-18 00:44:57 +0530

    Inheritance - tablePerClass

--- src/main/java/com/scaler/productservicefeb25/Inheritance/singleTable/User.java
+++ src/main/java/com/scaler/productservicefeb25/Inheritance/singleTable/User.java
@@ -1,10 +1,13 @@
 package com.scaler.productservicefeb25.Inheritance.singleTable;
 
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 public class User {
     private Long id;
     private String name;
